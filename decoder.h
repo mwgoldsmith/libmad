06f1f7303ca8e2b0f2335db5a3482969e8665fc2
mwgoldsmith <mike.w.goldsmith@gmail.com>
2016-02-06 03:05:31 +0000

    Formatting cleanup

--- decoder.h
+++ decoder.h
@@ -59,10 +59,8 @@ struct mad_decoder {
 
   enum mad_flow (*input_func)(void *, struct mad_stream *);
   enum mad_flow (*header_func)(void *, struct mad_header const *);
-  enum mad_flow (*filter_func)(void *,
-			       struct mad_stream const *, struct mad_frame *);
-  enum mad_flow (*output_func)(void *,
-			       struct mad_header const *, struct mad_pcm *);
+  enum mad_flow (*filter_func)(void *, struct mad_stream const *, struct mad_frame *);
+  enum mad_flow (*output_func)(void *, struct mad_header const *, struct mad_pcm *);
   enum mad_flow (*error_func)(void *, struct mad_stream *, struct mad_frame *);
   enum mad_flow (*message_func)(void *, void *, unsigned int *);
 };
